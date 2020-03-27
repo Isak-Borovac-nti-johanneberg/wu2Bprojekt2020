84c9d90574271e2bbcd8ffec05688c54f9ce1963
Isak-Borovac-nti-johanneberg <isak.borovac@gmail.com>
2020-03-27 17:38:05 +0100

    03-27
    Fixat en hamburgermeny med js

---
+++ js/burgar.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    var y = document.getElementById("burgarmenuitems");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+  }
+  } 
+
+
